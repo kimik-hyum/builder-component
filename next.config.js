ca0c3bb5836b30ff2be971c2d97fd0df77440e32
khyunjun <familykim01@naver.com>
2023-10-24 16:01:23 +0900

    builder 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const withBuilderDevTools = require("@builder.io/dev-tools/next")();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBuilderDevTools({
   env: {
     BUILDER_API_KEY: process.env.NEXT_PUBLIC_BUILDER_API_KEY,
-  }
-}
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
