96d01d14e20db9b48e90e1e1f2604b4ccb272289
khyunjun <familykim01@naver.com>
2023-10-24 15:37:55 +0900

    gitignore 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    BUILDER_API_KEY: process.env.NEXT_PUBLIC_BUILDER_API_KEY,
+  }
+}
 
 module.exports = nextConfig
